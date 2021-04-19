d3b8cbcc9e5107108e712ebc5ccb9534cc13297d
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-04-19 16:30:03 +0200

    Spikes and easy segments

---
+++ Assets/Scripts/Behaviours/Spikes2D.cs
@@ -0,0 +1,12 @@
+﻿namespace Game
+{
+    public class Spikes2D : AOnPlayerTriggerEnter2D
+    {
+        public float snareDuration;
+
+        protected override void OnPlayerEnter(Player player)
+        {
+            player.SetSnared(snareDuration);
+        }
+    }
+}
